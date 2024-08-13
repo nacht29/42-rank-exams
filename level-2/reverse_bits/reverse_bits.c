6bca1492fbb5fc9a8167f2eb2a65239be72e062e
nacht29 <nacht29.study@gmail.com>
2024-08-13 18:43:56 +0800

    update rev_bit

--- level-2/reverse_bits/reverse_bits.c
+++ level-2/reverse_bits/reverse_bits.c
@@ -1,5 +1,7 @@
 #include <unistd.h>
+#include <stdio.h>
 
+/*
 void reverse_bits(unsigned char octet)
 {
 	int i = 8;
@@ -14,8 +16,24 @@ void reverse_bits(unsigned char octet)
             write(1, "1", 1);
 	}
 }
+*/
+
+unsigned char	reverse_bits_og(unsigned char octet)
+{
+	int		i = 8;
+	unsigned char	res = 0;
+
+	while (i > 0)
+	{
+		res = res * 2 + (octet % 2);
+		octet = octet / 2;
+		i--;
+	}
+	return (res);
+}
 
 int main(void)
 {
-    reverse_bits(25);
+    // reverse_bits(25);
+	printf("og: %d\n", reverse_bits_og(3));
 }
